0cb63471d5e98be61bb21b5fc2919a816502f50d
Shallon <shallonmaria2001@gmail.com>
2025-04-20 21:14:07 +0300

    Edited requirement.txt

--- Dini/settings.py
+++ Dini/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i-mhm8u+&y4psf_simvdukwoley*+qn2(8sz*5h7_s2p%nke-x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
